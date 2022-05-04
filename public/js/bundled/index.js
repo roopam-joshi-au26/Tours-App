0f5190f09e4fa5a454ef7c5791e84a388bcbe362
roopam joshi <roopamjoshi63@gmail.com>
2022-05-05 01:31:23 +0530

    done log

--- public/js/bundled/index.js
+++ public/js/bundled/index.js
@@ -4436,6 +4436,7 @@ const login = async (email, password)=>{
             }, 1500);
         }
     } catch (err) {
+        console.log(err);
         _alerts.showAlert('error', err.response.data.message);
     }
 };
--- public/js/login.js
+++ public/js/login.js
@@ -20,6 +20,7 @@ export const login = async (email, password) => {
       }, 1500);
     }   
   }catch(err) {
+    console.log(err);
     showAlert('error',err.response.data.message);
   }
 };
